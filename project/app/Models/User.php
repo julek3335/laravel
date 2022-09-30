465a9994368ba04f37351f1e2cf00f292b075e83
Juliusz Hardyk <julek3335@gmail.com>
2022-09-30 15:19:23 +0200

    PI-26 Modiffy user model, userFactory, migration

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'driving_licence_category',
+        'status'
     ];
 
     /**
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // user belongs to company 
+    public function company() {
+        return $this->belongsTo(Company::class);
+    }
 }
