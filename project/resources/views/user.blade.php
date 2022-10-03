ed981fe0194ed75b39d54e6ab535f84c49ae5f65
Tymus <tymoteuszwa@gmail.com>
2022-10-03 17:58:27 +0200

    PI-44-v2 vehicles view

--- project/resources/views/user.blade.php
+++ project/resources/views/user.blade.php
@@ -1,10 +1,6 @@
 <h1>User test</h1>
-@foreach($users as $user)
 <tr>
-    <td> {{$user->id}} </td>
-    <td> {{$user->name}} </td>
-    <td> {{$user->email}} </td>
-    <td>{{$user->driving_licence_category}}</td>
-    <td> {{$user->status}} </td>
+    <td> {{$id}} </td>
+    <td> {{$name}} </td>
+
 </tr>
-@endforeach
