25ebfbf6f986f64d87b19aabb879efef65ba75e1
arekkopczynski <it.arekko@gmail.com>
2022-10-10 13:53:34 +0200

    Added modal for vehicle pickup

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -14,7 +14,23 @@
         <div class="card-body">
             <div class="row mb-2">
                 <div class="col-6 col-sm-4 col-md-3 col-xl-2">
-                    <x-adminlte-button label="Podejmij pojazd" icon="fas fa-light fa-plus"/>
+                    <x-adminlte-button label="Podejmij pojazd" icon="fas fa-light fa-plus" data-toggle="modal" data-target="#modalGetCar"/>
+                    <x-adminlte-modal id="modalGetCar" title="Podjęcie pojazdu" theme="light"
+                        icon="fas fa-bolt">
+                        <x-adminlte-select-bs name="carsList" label="Wybierz pojazd" 
+                            data-title="Wybierz pojazd ..." data-live-search
+                            data-live-search-placeholder="Wybierz pojazd ..." data-show-tick>
+                            <x-slot name="prependSlot">
+                                <div class="input-group-text bg-gradient-info">
+                                    <i class="fas fa-car-side"></i>
+                                </div>
+                            </x-slot>
+                            @foreach ($availableVehicles as $vehicle)
+                            <option data-icon="fa fa-fw fa-car">{{ $vehicle->name }}</option>
+                            @endforeach
+                        </x-adminlte-select-bs>
+                        <x-adminlte-button label="Podejmij pojazd" theme="success" class="float-right" icon="fas arrow-down"/>
+                    </x-adminlte-modal>
                 </div>
             </div>
         </div>
@@ -26,7 +42,7 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-6 col-sm-4 col-md-3 col-xl-2">
-                    <x-adminlte-info-box title="Pojazdy" text="15" icon="fas fa-light fa-car"/>
+                    <x-adminlte-info-box title="Pojazdy" text="{{ $numberOfVehicles }}" icon="fas fa-light fa-car"/>
                 </div>
             </div>
         </div>
