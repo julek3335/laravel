47a1dfa1106d645f7901720d3a454680164379f6
Juliusz Hardyk <julek3335@gmail.com>
2022-10-20 10:53:47 +0200

    PI-113 Add test user

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -3,14 +3,18 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use \App\Models\Vehicle;
-use \App\Models\Company;
 use \App\Models\User;
+use \App\Models\Company;
+use \App\Models\Vehicle;
 use \App\Models\Incident;
 use \App\Models\Insurance;
+use Illuminate\Support\Str;
 use \App\Models\Reservation;
+use App\Enums\UserStatusEnum;
+use Illuminate\Database\Seeder;
 use \App\Models\RegistrationCard;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 
 class DatabaseSeeder extends Seeder
@@ -22,10 +26,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        // Company::factory(5)
-        //     ->has(User::factory()-> count(3))
-        //     ->create();
+        //create test user Login: akowalski@mail.com Password: password
+        DB::table('users')->insert([
+            'name' => 'Adam',
+            'last_name' => 'Kowalski',
+            'email' => 'akowalski@mail.com',
+            'auth_level' => 0,
+            'company_id' => 1,
+            'email_verified_at' => now(),
+            'driving_licence_category' => 'B',
+            'status' => UserStatusEnum::FREE,
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
 
         $companys = Company::factory(5)
             -> has($users = User::factory(10)
