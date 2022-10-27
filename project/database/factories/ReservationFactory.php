05fb590b3458670250c8e93c8a11df203954e5d5
Juliusz Hardyk <julek3335@gmail.com>
2022-10-27 12:52:10 +0200

    Asign reservations to vehicles

--- project/database/factories/ReservationFactory.php
+++ project/database/factories/ReservationFactory.php
@@ -12,12 +12,10 @@ class ReservationFactory extends Factory
     public function definition()
     {
         $start_date = fake()->dateTimeBetween('-15 week', '+15 week');
-        $end_date = date_add($start_date,date_interval_create_from_date_string("2 days"));
         return [
-            // 'start_date' => fake()->dateTimeBetween('-5 week', '+6 week'),
-            // 'end_date' => fake()->dateTimeBetween('+1 week', '+7 week')
             'start_date' => $start_date,
-            'end_date' => $end_date
+            'end_date' => date_add($start_date,date_interval_create_from_date_string("2 days")),
+            'vehicle_id' => fake()->numberBetween(1, 125),
         ];
     }
 }
