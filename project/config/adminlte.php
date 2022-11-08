489b7e19fccad1b6d37811ff7fe550b95783c5cd
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-11-08 13:33:50 +0100

    PI-224 delete registration

--- project/config/adminlte.php
+++ project/config/adminlte.php
@@ -256,7 +256,7 @@ return [
     'dashboard_url' => '/',
     'logout_url' => 'logout',
     'login_url' => 'login',
-    'register_url' => 'register',
+    // 'register_url' => 'register',
     'password_reset_url' => 'password/reset',
     'password_email_url' => 'password/email',
     'profile_url' => '/user/showProfile',
