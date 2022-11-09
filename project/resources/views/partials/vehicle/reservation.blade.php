87886019150b0adf8f99b8a5ba9f435715a02ce5
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-11-09 01:50:14 +0100

    PI-232 reservation ajax

--- project/resources/views/partials/vehicle/reservation.blade.php
+++ project/resources/views/partials/vehicle/reservation.blade.php
@@ -1,61 +1,54 @@
-<x-adminlte-button label="Zarezerwuj pojazd" icon="fas fa-light fa-plus" data-toggle="modal" data-target="#modalReservationVehicle" id="modalReservationVehicleButton" />
+<x-adminlte-button id="openReservationButton" label="Zarezerwuj pojazd" icon="fas fa-light fa-plus" data-toggle="modal" data-target="#modalReservationVehicle" id="modalReservationVehicleButton" />
 
-<form action="/reservation-create" method="POST">
+<form id="formReservation" action="/reservation-create" method="POST" enctype="multipart/form-data">
     @csrf
+    @method('PUT')
     <x-adminlte-modal id="modalReservationVehicle" title="Rezerwacja pojazdu" theme="light" icon="fas fa-bolt">
-        <x-adminlte-select-bs name="vehicle_id" label="Wybierz pojazd" data-title="Wybierz pojazd ..." data-live-search data-live-search-placeholder="Wybierz pojazd ..." data-show-tick>
-            <x-slot name="prependSlot">
-                <div class="input-group-text bg-gradient-info">
-                    <i class="fas fa-car-side"></i>
-                </div>
-            </x-slot>
-            @if (isset($availableVehicles))
-            @foreach ($availableVehicles as $vehicle)
-                <option value="{{ $vehicle->id }}" data-icon="fa fa-fw fa-car">{{ $vehicle->name }}</option>
-            @endforeach
-            <option data-icon="fa fa-fw fa-car">Żaden pojazd nie jest dostępny</option>
-            @else
-                <option data-icon="fa fa-fw fa-car" selected>{{ $vehicle->name }}</option>
-            @endif
-        </x-adminlte-select-bs>
-
-        <!-- pokaz możliwość wyboru pracownika tylko dla Administratora albo edytora -->
-        @if($entitlements == 0 || $entitlements == 1)
-        <x-adminlte-select-bs name="user_id" label="Wybierz pracownika" data-title="Wybierz pojazd ..." data-live-search data-live-search-placeholder="Wybierz pojazd ..." data-show-tick>
-            <x-slot name="prependSlot">
-                <div class="input-group-text bg-gradient-info">
-                    <i class="fas fa-car-side"></i>
-                </div>
-            </x-slot>
-            @if (isset($avaibleUsers))
-            @foreach ($avaibleUsers as $user)
-                <option value="{{ $user->id }}" data-icon="fa fa-fw fa-car">{{ $user->name }} {{$user->last_name}}</option>
-            @endforeach
-            <option data-icon="fa fa-fw fa-car">Żaden pracownik nie jest dostępny</option>
-            @endif
-        </x-adminlte-select-bs>
-        @endif
-
         @php
         $config = ['format' => 'YYYY.MM.DD'];
         @endphp
 
-        <x-adminlte-input-date name="start_date" :config="$config" label="Data rozpoczęcia">
-            <x-slot name="start_date">
-                <x-adminlte-button icon="fas fa-calendar-day" title="Data rozpoczęcia" />
-            </x-slot>
-        </x-adminlte-input-date>
+        <div id="firstSection">
+            <x-adminlte-input-date id="startDate" name="start_date" :config="$config" label="Data rozpoczęcia">
+                <x-slot name="start_date">
+                    <x-adminlte-button icon="fas fa-calendar-day" title="Data rozpoczęcia" />
+                </x-slot>
+            </x-adminlte-input-date>
+            <x-adminlte-input-date id="endDate" name="end_date" :config="$config" label="Data zakończenia">
+                <x-slot name="end_date">
+                    <x-adminlte-button icon="fas fa-calendar-day" title="Data zakończenia" />
+                </x-slot>
+            </x-adminlte-input-date>
+            <x-adminlte-button id="submitDate" label="Zatwierdź daty" theme="success" class="mr-auto" />
+        </div>
 
-        <x-adminlte-input-date name="end_date" :config="$config" label="Data zakończenia">
-            <x-slot name="end_date">
-                <x-adminlte-button icon="fas fa-calendar-day" title="Data zakończenia" />
-            </x-slot>
-        </x-adminlte-input-date>
+        <div id="secondSection" style="display: none;">
+            <p><strong>Wybierz pojazd</strong></p>
+            <select name="vehicle_id" id="selectList" class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
+            </select>
 
-        <x-slot name="footerSlot">
-            <x-adminlte-button type="submit" label="Zarezerwuj pojazd" theme="success" class="mr-auto" icon="fas fa-arrow-alt-circle-right" />
-            <x-adminlte-button theme="danger" label="Zamknij" data-dismiss="modal" />
-        </x-slot>
+            <!-- pokaz możliwość wyboru pracownika tylko dla Administratora albo edytora -->
+            @if($entitlements == 0 || $entitlements == 1)
+            <x-adminlte-select-bs name="user_id" label="Wybierz pracownika" data-title="Wybierz pojazd ..." data-live-search data-live-search-placeholder="Wybierz pojazd ..." data-show-tick>
+                <x-slot name="prependSlot">
+                    <div class="input-group-text bg-gradient-info">
+                        <i class="fas fa-car-side"></i>
+                    </div>
+                </x-slot>
+                @if (isset($avaibleUsers))
+                @foreach ($avaibleUsers as $user)
+                <option value="{{ $user->id }}" data-icon="fa fa-fw fa-car">{{ $user->name }} {{$user->last_name}}</option>
+                @endforeach
+                <option data-icon="fa fa-fw fa-car">Żaden pracownik nie jest dostępny</option>
+                @endif
+            </x-adminlte-select-bs>
+            @endif
+
+            <x-adminlte-button id="submitButton" type="submit" label="Zarezerwuj pojazd" theme="success" class="mr-auto" icon="fas fa-arrow-alt-circle-right" />
+            <x-adminlte-button id="return" label="Cofnij" class="mr-auto" />
+            <x-slot name="footerSlot">
+            </x-slot>
+        </div>
     </x-adminlte-modal>
 </form>
 @section('js')
@@ -64,6 +57,41 @@
     $("#modalPickupVehicleButton").click(function() {
         $('#startDate').datetimepicker("defaultDate", new Date());
     });
+
+    $("#return").click(function() {
+        $('#firstSection').css('display', 'block')
+        $('#secondSection').css('display', 'none')
+    })
+
+    $(document).ready(function() {
+        $('#submitDate').click(function() {
+            
+            var date_start = $('start_date').val()
+            var date_end = $('start_date').val()
+
+            if(!$('#startDate').val() || !$('#endDate').val()){
+                alert('Prosimy uzupełnić obydwie daty')
+                return
+            }
+
+            $.ajax({
+                url: '/reservation/available-cars',
+                method: 'GET',
+                data: {
+                    start_date: date_start,
+                    end_date: date_end
+                },
+                success: function(data) {
+                    $('#firstSection').css('display', 'none')
+                    $('#secondSection').css('display', 'block')
+                    data.forEach(element => {
+                        $('#selectList').append('<option value="' + element.id + '"> ' + element.license_plate + '</option>');
+                    });
+                }
+            })
+        })
+
+    })
 </script>
 @parent
 @stop
