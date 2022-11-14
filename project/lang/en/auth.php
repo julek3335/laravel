5b3c07ed126691a9586fb20b2d174a383cf7b6ba
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-11-14 18:37:01 +0100

    validation login text

--- project/lang/en/auth.php
+++ project/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'nie posiadamy takich danych.',
+    'password' => 'Niepoprawne hasło.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
