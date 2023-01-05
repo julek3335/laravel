467ba174d838bd12601aaa5134e3b00fae0875fe
Juliusz Hardyk <julek3335@gmail.com>
2023-01-05 14:17:36 +0100

    Fix translation

---
+++ project/resources/lang/pl/status.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'READY' => 'Gotowy',
+    'IN_USE' => 'W użyciu',
+    'RESERVED' => 'Rezerwacja',
+    'MAINTENANCE' => 'W trakcie serwisu',
+    'FREE' => 'Wolny',
+    'INACTIVE' => 'Nieaktywne',
+    'ACTIVE' => 'Aktywne',
+    'RENTING' => 'Zajęty',
+    'IN_PROGRESS' => 'W trakcie',
+    'resolved' => 'Rozwiązane',
+    'FINISHED' => 'Zakończone',
+    'in_progress' => "W trakcie",
+    'unprocessed' => "Nieprzetworzony",
+    'RENTING' => 'W trasie',
+];
